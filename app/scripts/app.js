8c15bffc6439d131daeb7f64fb39943a35065801
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-05-01 20:46:26 +0100

    Initialized project using Yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nodeJwtWorkshopApp
+ * @description
+ * # nodeJwtWorkshopApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nodeJwtWorkshopApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nodeJwtWorkshopApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nodeJwtWorkshopApp
+ */
+angular.module('nodeJwtWorkshopApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
